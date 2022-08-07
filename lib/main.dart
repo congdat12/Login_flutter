d98490a7ec4d9ee3ec4d99735b4235284d7a887a
congdat12 <congdat1291995@gmail.com>
2022-08-07 14:39:45 +0700

    calculator_ui

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:excersise_login/login.dart';
 import 'package:flutter/material.dart';
 
+import 'calculator_ui.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -13,6 +15,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -23,7 +26,7 @@ class MyApp extends StatelessWidget {
         // or simply save your changes to "hot reload" in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
-        primarySwatch: Colors.blue,
+        //primarySwatch: Colors.red ,
       ),
       home:  LoginScreen(),
     );
